7099227af89d6bb8707e54113a7df7868176d641
aiXing <862099730@qq.com>
2015-11-20 15:58:58 +0800

    Initial Commit

---
+++ AXPracticalHUD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AXPracticalHUD
+//
+//  Created by ai on 15/11/20.
+//  Copyright © 2015年 AiXing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AXPracticalHUD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AXPracticalHUD
+//
+//  Created by ai on 15/11/20.
+//  Copyright © 2015年 AiXing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
