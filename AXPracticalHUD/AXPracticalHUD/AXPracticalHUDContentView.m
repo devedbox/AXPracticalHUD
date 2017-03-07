9dd4a1de580ae0a3a6f56bebc2406aa0106073ea
devedbox <devedbox@gmail.com>
2017-03-07 13:57:54 +0800

    Added license and comments content.

--- AXPracticalHUD/AXPracticalHUD/AXPracticalHUDContentView.m
+++ AXPracticalHUD/AXPracticalHUD/AXPracticalHUDContentView.m
@@ -5,6 +5,23 @@
 //  Created by ai on 9/7/15.
 //  Copyright © 2015 ai. All rights reserved.
 //
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
 
 #import "AXPracticalHUDContentView.h"
 
@@ -13,6 +30,7 @@
 #endif
 
 @interface AXPracticalHUDContentView()
+/// Effects view for the content view.
 @property(strong, nonatomic) UIView *effectView;
 @end
 
@@ -48,7 +66,7 @@
     
     if (_translucent) {
         CGContextSetFillColorWithColor(context, [UIColor clearColor].CGColor);
-    } else if (_endColor) {
+    } else if (_endColor && _color) {
         // Gradient color
         size_t locationCount = 2;
         CGFloat gradLocations[2] = {0.0, 1.0};
@@ -73,7 +91,7 @@
         } else {
             CGContextSetFillColorWithColor(context, _color.CGColor);
         }
-        
+        // Set the cornor radius of the content view.
         CGContextBeginPath(context);
         CGRect rect = self.bounds;
         CGContextMoveToPoint(context, CGRectGetMinX(rect) + 0.0, CGRectGetMinY(rect));
