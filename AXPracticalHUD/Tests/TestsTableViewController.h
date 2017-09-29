4fb1f188f41bfc838de522d5f933437a9ed1b375
aixing-hb <aixing@hydsoft.com>
2017-09-29 14:31:59 +0800

    Added TestsTableViewController.

---
+++ AXPracticalHUD/Tests/TestsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestsTableViewController.h
+//  AXPracticalHUD
+//
+//  Created by devedbox on 2017/5/16.
+//  Copyright © 2017年 AiXing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestsTableViewController : UITableViewController
+
+@end
---
+++ AXPracticalHUD/Tests/TestsTableViewController.m
@@ -0,0 +1,32 @@
+//
+//  TestsTableViewController.m
+//  AXPracticalHUD
+//
+//  Created by devedbox on 2017/5/16.
+//  Copyright © 2017年 AiXing. All rights reserved.
+//
+
+#import "TestsTableViewController.h"
+
+@interface TestsTableViewController ()
+
+@end
+
+@implementation TestsTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
