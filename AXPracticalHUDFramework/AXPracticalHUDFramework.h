6e5058185c850a425003f5efdd7ae7a5ca718b6c
aixing-hb <aixing@hydsoft.com>
2017-09-29 15:30:59 +0800

    Added carthage framework target.

---
+++ AXPracticalHUDFramework/AXPracticalHUDFramework.h
@@ -0,0 +1,19 @@
+//
+//  AXPracticalHUDFramework.h
+//  AXPracticalHUDFramework
+//
+//  Created by devedbox on 2017/9/29.
+//  Copyright © 2017年 AiXing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AXPracticalHUDFramework.
+FOUNDATION_EXPORT double AXPracticalHUDFrameworkVersionNumber;
+
+//! Project version string for AXPracticalHUDFramework.
+FOUNDATION_EXPORT const unsigned char AXPracticalHUDFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AXPracticalHUDFramework/PublicHeader.h>
+
+
